a366ab8a1b78f4114acf7a4c5229c2856375b777
TaroKamikaze <ms06saz@gmail.com>
2018-08-02 15:45:21 +0900

    wip

--- main.go
+++ main.go
@@ -3,20 +3,20 @@ package main
 import (
 	"fmt"
 	"os"
+	"strings"
 )
 
 func main() {
-  // このコードは引数と標準出力を用いたサンプルコードです。
-  // このコードは好きなように編集・削除してもらって構いません。
-  // ---
-  // This is a sample code to use arguments and outputs.
-  // Edit and remove this code as you like.
+	// このコードは引数と標準出力を用いたサンプルコードです。
+	// このコードは好きなように編集・削除してもらって構いません。
+	// ---
+	// This is a sample code to use arguments and outputs.
+	// Edit and remove this code as you like.
 
 	run(os.Args[1:])
 }
 
 func run(args []string) {
-	for i, v := range args {
-		fmt.Printf("argv[%d]: %s\n", i, v)
-	}
+	s := strings.Join(args, " ")
+	fmt.Printf("Hello %s!", s)
 }
